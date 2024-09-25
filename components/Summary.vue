b99bbb5e0573794f167766c5e91edd1500d4c800
Serg Puhoff <serg@fast-report.com>
2024-09-25 17:05:37 +0400

    add block summary

---
+++ components/Summary.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col justify-between gap-6 lg:flex-row">
+    <div class="flex-1 rounded-xl bg-gray-900 p-6">
+      <p class="text-preset-4 mb-3 text-white">Current Balance</p>
+      <p class="text-preset-1 font-bold text-white">$4,836.00</p>
+    </div>
+    <div class="flex-1 rounded-xl bg-white p-6">
+      <p class="text-preset-4 mb-3 text-gray-500">Income</p>
+      <p class="text-preset-1 font-bold text-gray-900">$3,814.25</p>
+    </div>
+    <div class="flex-1 rounded-xl bg-white p-6">
+      <p class="text-preset-4 mb-3 text-gray-500">Expenses</p>
+      <p class="text-preset-1 font-bold text-gray-900">$1,700.50</p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 Transactions
 <template>
-  <div class="pl-10 pt-10">
-    <h1 class="text-preset-1 font-bold text-gray-800">Overview</h1>
+  <div class="p-10">
+    <h1 class="text-preset-1 mb-10 font-bold text-gray-800">Overview</h1>
+    <Summary />
   </div>
 </template>
